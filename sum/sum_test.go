c5e8182c73769e3a3ecbec33169bcff64e51d1fc
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-04-18 22:14:43 +0200

    gobench: naive improvement for the simple test case (sum)

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -9,4 +9,14 @@ func TestInts(t *testing.T) {
 	if s != 15 {
 		t.Errorf("Sum of one to five should be 15; got %v", s)
 	}
+
+	s = Ints()
+	if s != 0 {
+		t.Errorf("Sum of none should be 0; got %v", s)
+	}
+
+	s = Ints(1, -1)
+	if s != 0 {
+		t.Errorf("Sum of one and minus one should be 0; got %v", s)
+	}
 }
