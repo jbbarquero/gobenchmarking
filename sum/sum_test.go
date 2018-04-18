708cc7aa04ad7f3e2afe44c88b50581d1e209715
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-04-19 00:15:01 +0200

    gobench: separate package for tests

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,21 +1,23 @@
-package sum
+package sum_test
 
 import (
 	"testing"
+
+	"github.com/jbbarquero/gobenchmarking/sum"
 )
 
 func TestInts(t *testing.T) {
-	s := Ints(1, 2, 3, 4, 5)
+	s := sum.Ints(1, 2, 3, 4, 5)
 	if s != 15 {
 		t.Errorf("Sum of one to five should be 15; got %v", s)
 	}
 
-	s = Ints()
+	s = sum.Ints()
 	if s != 0 {
 		t.Errorf("Sum of none should be 0; got %v", s)
 	}
 
-	s = Ints(1, -1)
+	s = sum.Ints(1, -1)
 	if s != 0 {
 		t.Errorf("Sum of one and minus one should be 0; got %v", s)
 	}
@@ -34,11 +36,10 @@ func TestIntsBis(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			s := Ints(tc.numbers...)
+			s := sum.Ints(tc.numbers...)
 			if s != tc.sum {
 				t.Fatalf("Test %v failed. Sum of %v should be %v; got %v", tc.name, tc.numbers, tc.sum, s)
 			}
 		})
-
 	}
 }
