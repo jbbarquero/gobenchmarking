3fb933f25c6537352ec8f5bc66bf148420de6371
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-05-04 01:25:51 +0200

    gobench: nil channels, fully functional code

--- nilchannels/main.go
+++ nilchannels/main.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
 
 func main() {
 	a := asChan(1, 3, 5, 7)
-	for v := range a {
+	b := asChan(2, 4, 6, 8)
+	c := merge(a, b)
+	for v := range c {
 		fmt.Println(v)
 	}
+}
+
+func merge(a, b <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		for a != nil || b != nil {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					a = nil
+					log.Printf("a is done")
+					continue
+				}
+				c <- v
+			case v, ok := <-b:
+				if !ok {
+					b = nil
+					log.Printf("b is done")
+					continue
+				}
+				c <- v
+			}
+		}
+	}()
+	return c
 }
 
 func asChan(vs ...int) <-chan int {
